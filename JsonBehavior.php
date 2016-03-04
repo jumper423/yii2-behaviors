7b54b2c97b225987d9d6d5fb90292abe9474fd49
Punk <jump.e.r@yandex.ru>
2016-03-04 23:28:28 +0300

    JsonBehavior

---
+++ JsonBehavior.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace jumper\behaviors;
+
+use yii\behaviors\AttributeBehavior;
+use yii\helpers\Json;
+
+class JsonBehavior extends AttributeBehavior
+{
+    /**
+     * @inheritdoc
+     */
+    public function evaluateAttributes($event)
+    {
+        if (!empty($this->attributes[$event->name])) {
+            $attributes = (array) $this->attributes[$event->name];
+            foreach ($attributes as $attribute) {
+                if (is_string($attribute)) {
+                    if (!is_string($this->owner->$attribute) && !is_null($this->owner->$attribute)) {
+                        $this->owner->$attribute = Json::encode($this->owner->$attribute);
+                    }
+                }
+            }
+        }
+    }
+}
